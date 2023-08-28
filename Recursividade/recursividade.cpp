c3303c8a4b1b59d9269f3314fb8fae2099f02795
wesley.r <wesley.r@perkons.com>
2023-08-28 10:47:54 -0300

    recursividade

--- Recursividade/recursividade.cpp
+++ Recursividade/recursividade.cpp
@@ -7,7 +7,7 @@ void conter_with_recursion(int num, int cont);
 
 int main() {
 
-    // conter_without_recursion(10);
+    conter_without_recursion(10);
     conter_with_recursion(10, 2);
 
     return 0;
@@ -18,13 +18,13 @@ void conter_without_recursion(int num){
     cout << "Without recursion" << "\n";
 
     for(int i = 0; i<num; i++){
-        cout << i << "\n";
+        cout << "without: " << i << "\n";
     }
 }
 
 void conter_with_recursion(int num, int cont){
 
-    cout << cont << "\n";
+    cout << "with: " << cont << "\n";
 
     if(num > cont){
         /* É feito um pré incremento na variável cont para não entra em loop infinito.
